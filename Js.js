228b13fe33496e9b1d3ab17015488d543bbece7f
Avatar-shop <110927462+Azizaga2001@users.noreply.github.com>
2022-11-11 18:48:37 +0400

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,49 @@
+const cars = ['Banana', 'Apple', 'Carrot', 'Mango']
+
+//1
+for(let i = 0; i < 4; i+=1){
+    console.log(cars[i])
+}
+
+console.log('        ')
+
+//2
+for(let i = 0; i < 10; i+=2){
+    console.log('hello')
+}
+
+console.log('        ')
+
+//3
+for(let i = 0; i < cars.length; i+=1){
+    console.log(cars[i])
+}
+
+console.log('        ')
+
+//4
+
+let str = 'Hello'
+console.log(str.charAt(0))
+
+console.log('        ')
+
+//5
+
+for(let i = 0; i < 10; i+=1){
+    console.log('Hello World')
+}
+
+console.log('        ')
+
+//6
+
+for(let i = 0; i < 10; i+=2){
+    console.log('Hello World')
+}
+
+
+for(let i = 9; i < 10; i+=2){
+    console.log('A')
+}
+
